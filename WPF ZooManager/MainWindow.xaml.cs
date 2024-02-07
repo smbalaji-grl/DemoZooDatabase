77dc164952cb59897385645d0b99cd44f779f50c
smbalaji-grl <balaji.m@graniteriverlabs.in>
2024-02-07 23:43:58 +0530

    database connection with WPF project
    server exploerer connection with solution explorere
    using conection string
    adding lable and list box in  in xaml file
    adding sqlconnection extension class and sqladapter class to get or fetch data from sql server .
    DataTable class to populate the fetched data from table in to virtual datatable for c#(DataTable class)

--- WPF ZooManager/MainWindow.xaml.cs
+++ WPF ZooManager/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace WPF_ZooManager
 {
@@ -21,12 +23,39 @@ namespace WPF_ZooManager
     /// </summary>
     public partial class MainWindow : Window
     {
+        SqlConnection conn;
         public MainWindow()
         {
             InitializeComponent();
 
             string connectionString = ConfigurationManager.ConnectionStrings["WPF_ZooManager.Properties.Settings.DemoDBConnectionString"].ConnectionString;
+            conn = new SqlConnection(connectionString);
+            showZoos();
 
         }
+        private void showZoos()
+        {
+            try
+            {
+                string showAllDataInZooQuery = "select * from Zoo";
+                SqlDataAdapter adapter = new SqlDataAdapter(showAllDataInZooQuery, conn);
+
+                using (adapter)
+                {
+                    DataTable zooTable = new DataTable();
+                    adapter.Fill(zooTable);
+                    listZoos.DisplayMemberPath = "Location";
+                    listZoos.SelectedValuePath = "ID";
+                    listZoos.ItemsSource = zooTable.DefaultView;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
     }
+
+
 }
